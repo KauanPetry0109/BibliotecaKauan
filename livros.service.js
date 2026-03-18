b0fbea5563a7e11a1a1f9252d806fed991591ed3
KauanPetry <kauanpetry01092009@gmail.com>
2026-03-18 09:22:58 -0300

    merolhado

---
+++ livros.service.js
@@ -0,0 +1,23 @@
+const acervo = [
+    {
+        id: 1,
+        titulo: 'O Senhor dos Anéis',
+        autor: 'J.R.R. Tolkien',
+        ano: 1954,
+        disponivel: true
+    },
+    {
+        id: 2,
+        titulo: "Assassin's Creed",
+        autor: 'Oliver Bowden',
+        ano: 2012,
+        disponivel: true
+    },
+    {
+        id: 3,
+        titulo: 'Altered Carbon',
+        autor: 'Richard K. Morgan',
+        ano: 2002,
+        disponivel: false
+    }
+]
